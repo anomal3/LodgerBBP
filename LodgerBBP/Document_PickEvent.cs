5eeebf5ca80a879ef7be82c3ccb299329ce54754
Roman <anomal3@yandex.ru>
2021-11-29 13:51:57 +0400

    Добавлена функция контекстного меню для аппартаментов

--- LodgerBBP/Document_PickEvent.cs
+++ LodgerBBP/Document_PickEvent.cs
@@ -32,11 +32,15 @@ namespace LodgerBBP
             EHLV.ClearItems();
 
             UIDocument uidoc = uiapp.ActiveUIDocument;
+            Data.UIApplication = uiapp;
+            Data.ActiveUIDocument = Data.UIApplication.ActiveUIDocument;
             if (null == uidoc)
             {
                 return; // no document, nothing to do
             }
             Document doc = uidoc.Document;
+            Data.ActiveDocument = doc;
+            Data.ActiveUIDocument = uiapp.ActiveUIDocument;
             using (Transaction tx = new Transaction(doc))
             {
                 tx.Start("DocumentPickEvent");
@@ -55,9 +59,11 @@ namespace LodgerBBP
                 Selection sel = uidoc.Selection;
 
                 #region Выбор цвета https://thebuildingcoder.typepad.com/blog/2019/09/whats-new-in-the-revit-20201-api.html#2.5.1
-                Helper.SelectionColor(128,255,64,true);
-                Helper.PreselectionColor(255,0,0);
+                //Helper.SelectionColor(128,255,64,true);
+                //Helper.PreselectionColor(255,0,0);
                 #endregion
+                Helper.SelectionColor(255,10,0, true);
+                Helper.PreselectionColor(200, 0, 0);
 
                 IList<Reference> objRefsToCopy = sel.PickObjects(ObjectType.Element, "Выберите помещения для добавления в коллекцию");
                
@@ -81,11 +87,12 @@ namespace LodgerBBP
                         double dArea = ExactM2Area;
                         //EHLV.AddToList(element2Ref.Name, strArea, ExactM2Area);
                         new Helper().RoomTypeDefinition(element2Ref.get_Parameter(BuiltInParameter.ROOM_NAME).AsString());
-                        EHLV.AddToObserverCollection(element2Ref.get_Parameter(BuiltInParameter.ROOM_NAME).AsString(), dArea, ExactM2Area, elemIdList, r.ElementId);
+                        EHLV.AddToObserverCollection(element2Ref.get_Parameter(BuiltInParameter.ROOM_NAME).AsString(), dArea, ExactM2Area, r.ElementId);
                     }
-
                     //ActDP?.Invoke(this, new Document_PickEventArgs(element2Ref.Name));
                 }
+
+                
 
                 uidoc.Selection.SetElementIds(elemIdList);
                 uidoc.ShowElements(elemIdList);
